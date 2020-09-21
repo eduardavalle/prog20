b0b46db2a931d0ae65f464e32bb712f79a15757d
eduardavalle <61236184+eduardavalle@users.noreply.github.com>
2020-09-21 09:53:00 -0300

    Add files via upload

---
+++ front_end/scripts.js
@@ -0,0 +1,22 @@
+$(function() {
+    $.ajax({
+        url: 'http://localhost:5000/listagem_sapatos',
+        method: 'GET',
+        dataType: 'json',
+        success: listarSapato,
+        error: function() {
+            alert("ERRO");
+        }
+    });
+  
+    function listarSapato(sapatos) {
+        for (sapato of sapatos) {
+            novaLinha = `<tr>
+                        <td>${sapato.modelo}</td>
+                        <td>${sapato.marca}</td>
+                        <td>${sapato.cor}</td>
+                      </tr>`;
+            $('#tabelaSapato').append(novaLinha);
+        }
+    }
+ });
