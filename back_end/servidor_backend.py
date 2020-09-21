88ea286b3cef4e05a765c430cad74ff6b4e28b05
eduardavalle <61236184+eduardavalle@users.noreply.github.com>
2020-09-21 09:58:00 -0300

    Add files via upload

---
+++ back_end/servidor_backend.py
@@ -0,0 +1,19 @@
+from config import *
+from modelo_sapato import Sapato
+
+@app.route("/")
+def inicio():
+    return 'Sistema de cadastro de sapatos. '+\
+        '<a href="/listagem_sapatos">Operação listar</a>'
+
+@app.route("/listagem_sapatos")
+def listagem_sapatos():
+    sapato = db.session.query(Sapato).all()
+    sapato_em_json = [ sapato.json() for sapato in all ]
+    resposta = jsonify(sapato_em_json)
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta 
+    
+
+app.run(debug=True)
+
